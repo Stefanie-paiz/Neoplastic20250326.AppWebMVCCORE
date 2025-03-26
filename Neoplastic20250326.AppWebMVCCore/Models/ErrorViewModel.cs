2837d83c142657f78b68cfac369d62d51a57a5d2
Azucena Sanchez <Chelitaazu@outlook.com>
2025-03-26 16:50:04 -0600

    Agregar archivos de proyecto.

---
+++ Neoplastic20250326.AppWebMVCCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Neoplastic20250326.AppWebMVCCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
